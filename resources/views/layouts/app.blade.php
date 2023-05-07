ec2971e47053c11cf463003bb4224d699a3f0537
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2023-05-07 17:27:35 +0100

    WIP

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="assets/plugins/aos/aos.css">
+
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <body class="home-page body-five">
+        <div class="main-wrapper">
+            @include('partials.header')
+            @yield('content')
+            @include('partials.footer')
+        </div>
+        <script data-cfasync="false"
+            src="https://kofejob.dreamguystech.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+        <script src="assets/js/owl.carousel.min.js"></script>
+
+        <script src="assets/plugins/aos/aos.js"></script>
+
+        <script src="assets/plugins/select2/js/select2.min.js"></script>
+
+        <script src="assets/js/slick.js"></script>
+
+        <script src="assets/js/script.js"></script>
+    </body>
+
+</html>
